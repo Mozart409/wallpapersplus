5f35d2bbc0ee9e6c94d667221b7805ba248495ea
Amadeus <maderamadeus@gmail.com>
2019-09-30 11:29:27 +0200

    BackTop Responsiv

--- src/components/ToTop.js
+++ src/components/ToTop.js
@@ -4,7 +4,7 @@ import ScrollToTop from "react-scroll-up";
 function ToTop() {
   return (
     <ScrollToTop showUnder={160}>
-      <span className="bg-blue-700 text-white p-2 -mr-2 sm:-mr-6 p-8 sm:p-2 xs:p-1 round hover:shadow-xl">
+      <span className="bg-blue-700 text-white p-1 xs:p-1 sm:p-2 md:p-4 lg:p-6 xl:p-8 invisible md:visible lg:visible xl:visible rounded hover:shadow-xl">
         UP
       </span>
     </ScrollToTop>
