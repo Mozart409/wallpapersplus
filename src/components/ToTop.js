98a2a356daa940a5ad7c6439731b200aa952c756
Mozart409 <mozart_amadeus@yahoo.com>
2019-10-23 11:55:54 +0200

    updated css for BackTop Button

--- src/components/ToTop.js
+++ src/components/ToTop.js
@@ -4,13 +4,13 @@ import ScrollToTop from "react-scroll-up";
 function ToTop() {
   return (
     <div>
-    <ScrollToTop showUnder={160} style={{right: 0}} >
-      <div className="bg-blue-700 text-white rounded px-2 py-1">
+    <ScrollToTop showUnder={160} style={{right: 5, position:"fixed"}} >
+    
       <span 
       className="UprightText">
        TOP
       </span>
-      </div>
+     
     </ScrollToTop>
     </div>
   );
